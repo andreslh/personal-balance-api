e618743b0437436cf123fdbb6c19207759c0c0e5
Andres Ledesma <andres_ledesma@epam.com>
2020-04-08 15:38:00 +0200

    Cors added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const apiRouter = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(apiRouter);
 
